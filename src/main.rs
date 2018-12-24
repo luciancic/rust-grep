22e5524ef83f0a40c8c0ed597c319a2ada9a795e
luciancic <luciancondrea95@gmail.com>
2018-12-23 21:30:01 -0500

    Refactor command line arg extraction logic

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,26 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for {}", query);
-    println!("Searching in file {}", filename);
+    let config = Config::new(&args);
 
-    let file_content =  fs::read_to_string(filename)
+    println!("Searching for {}", config.query);
+    println!("Searching in file {}", config.filename);
+
+    let file_content =  fs::read_to_string(config.filename)
         .expect("Could not read the file.");
     println!("Full file text:\n{}", file_content);
+}
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Self {
+        Self {
+            query: args[1].clone(),
+            filename: args[2].clone()
+        }
+    }
 }
